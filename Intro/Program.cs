2392d94d91d183cbc4d45f44c6f54cd2298e8e26
David Jorge-Bates <jorgebd@stpaulsschool.org.uk>
2023-09-04 09:57:50 +0100

    change to greeting

--- Intro/Program.cs
+++ Intro/Program.cs
@@ -4,7 +4,7 @@
     {
         static void Main()
         {
-            Console.WriteLine("Hello, World!");
+            Console.WriteLine("Hello, L8!");
             int number = Simple(4);
             Console.Write(number);
         }
