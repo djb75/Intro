3a524fa2226209b5adcc5b6b393bfc0b6b2c777a
David School <David School@DESKTOP-IT4BCPM>
2023-09-04 09:52:09 +0100

    Add project files.

---
+++ Intro/Program.cs
@@ -0,0 +1,17 @@
+﻿namespace Intro
+{
+    internal class Program
+    {
+        static void Main()
+        {
+            Console.WriteLine("Hello, World!");
+            int number = Simple(4);
+            Console.Write(number);
+        }
+
+        static int Simple(int number)
+        {
+            return number * 2;
+        }
+    }
+}
